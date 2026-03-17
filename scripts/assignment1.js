807d27209a112bddb93b5b271075e0011980f680
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-17 11:15:45 +0100

    add exercise 1 & 2

---
+++ scripts/assignment1.js
@@ -0,0 +1,15 @@
+
+// print hello world
+
+console.log("Hello, World!");
+
+// Create variables
+
+let fName = "Achini Ishani";
+let age = 32;
+let favColor = "red";
+
+console.log ("my name is " + fName + " , I'm " + age + " years old and my favorite color is " + favColor );
+
+// Basic arithmetic operations
+
