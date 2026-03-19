b12dad1426fb40591c0d850bff46d9af03523d90
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-19 22:33:11 +0100

    add js to assignment2

--- scripts/assignment2.js
+++ scripts/assignment2.js
@@ -1,3 +1,13 @@
 function priceFunction() {
-    let amount = prompt ("Please enter the price of the item (ex:- $ 100.00)")
+    let amount = prompt ("Please enter the price of the item (ex:- $ 100.00)");
+    // reomove $ and spaces
+    amount = amount.replace("$" , "").trim();
+    // convert to number
+    let newAmount = parseFloat(amount);
+    // add discount 
+    let discountedPrice = newAmount - (newAmount * 0.10);
+    // controls decimal places
+    document.getElementById("price").innerHTML = `Your New Price is ${discountedPrice.toFixed(2)}`;
+    
+   
 }
