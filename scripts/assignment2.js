845e9faf40eade8fcbb8637b65a3a3c3b278cc80
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-19 22:40:04 +0100

    add styles to the card

--- scripts/assignment2.js
+++ scripts/assignment2.js
@@ -1,13 +1,12 @@
 function priceFunction() {
-    let amount = prompt ("Please enter the price of the item (ex:- $ 100.00)");
-    // reomove $ and spaces
-    amount = amount.replace("$" , "").trim();
-    // convert to number
-    let newAmount = parseFloat(amount);
-    // add discount 
-    let discountedPrice = newAmount - (newAmount * 0.10);
-    // controls decimal places
-    document.getElementById("price").innerHTML = `Your New Price is ${discountedPrice.toFixed(2)}`;
-    
-   
+  let amount = prompt("Please enter the price of the item (ex:- $ 100.00)");
+  // reomove $ and spaces
+  amount = amount.replace("$", "").trim();
+  // convert to number
+  let newAmount = parseFloat(amount);
+  // add discount
+  let discountedPrice = newAmount - newAmount * 0.1;
+  // controls decimal places
+  document.getElementById("price").innerHTML =
+    `Your New Price is ${discountedPrice.toFixed(2)}`;
 }
