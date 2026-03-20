657779e7a44133c2da4fb365cd0b88366e1206a6
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-20 09:21:24 +0100

    Complete JS file

--- scripts/assignment2.js
+++ scripts/assignment2.js
@@ -10,3 +10,25 @@ function priceFunction() {
   document.getElementById("price").innerHTML =
     `Your New Price is ${discountedPrice.toFixed(2)}`;
 }
+
+
+function discountFunction(){
+    let amount = prompt ("Please enter the price of the item (ex:- $ 100.00)");
+    amount = amount.replace("$", " ").trim();
+    let newAmount = parseFloat(amount);
+
+    let discount = prompt ("Enter discount percentage (ex:10%):");
+    discount = discount.replace("%", " ").trim();
+    let newDiscount= parseFloat(discount);
+
+    let discountAmount = newAmount * (newDiscount/100);
+    
+    let priceAmount = newAmount - discountAmount; 
+
+    document.getElementById("discount").innerHTML = 
+    `Original Price: ${newAmount.toFixed(2)} <br>
+     Discount : ${newDiscount}% <br>
+     You save : ${discountAmount.toFixed(2)} <br>
+     Your New Price is : ${priceAmount.toFixed(2)}`;
+
+}
